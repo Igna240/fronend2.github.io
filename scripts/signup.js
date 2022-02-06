3edcb58b1d265d4a9d8e3d9babd01a30023d91e1
unknown <brezanignacio@gmail.com>
2022-02-06 20:05:35 -0300

    s

--- scripts/signup.js
+++ scripts/signup.js
@@ -132,8 +132,8 @@ window.addEventListener("load", function () {
       })
       .then(function (token) {
         localStorage.setItem("token", token.jwt);
-        /* window.location.href = "/proyectoIntegrador/mis-tareas.html"; */
-        window.location.href = "http://127.0.0.1:5500/mis-tareas.html";
+        window.location.href = "/proyectoIntegrador/mis-tareas.html";
+        /* window.location.href = "http://127.0.0.1:5500/mis-tareas.html"; */
       })
       .catch(function (e) {
         console.log(e);
