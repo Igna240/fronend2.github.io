96b1b64ff813c277913ae151024b4dfaaf496f1e
unknown <brezanignacio@gmail.com>
2022-02-06 23:30:18 -0300

    pruebo url

--- scripts/index.js
+++ scripts/index.js
@@ -71,8 +71,9 @@ window.addEventListener("load", function () {
         })
         .then(function (token) {
           sessionStorage.setItem("token", token.jwt);
-          window.location.href = "http://127.0.0.1:5500/mis-tareas.html";
+          /* window.location.href = "http://127.0.0.1:5500/mis-tareas.html"; */
           /* window.location.href = "/proyectoIntegrador/mis-tareas.html"; */
+          window.location.href = "https://igna240.github.io/fronend2.github.io/mis-tareas.html";
         })
         .catch(function (e) {
           console.log(e);
